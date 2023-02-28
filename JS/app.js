4d0e2f48333c661cb3f32eb7701dc9f1913a2fc7
Dasha Burgos <dasha.lewis@gmail.com>
2023-02-27 19:42:45 -0500

    click event listener for game start

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+
+let playButton = document.getElementById('playButton');
+function handlePlayButton(event){
+  let startPlay = event.target.id;
+
+  window.location.replace('gamePlay.html');
+
+}
+
+playButton.addEventListener('click', handlePlayButton);
