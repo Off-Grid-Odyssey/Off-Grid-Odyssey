07b41ed81e648bf8699c3958b9ee49938ff63814
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-03-01 17:37:23 -0600

    spelling

--- JS/highScore.js
+++ JS/highScore.js
@@ -1,11 +1,11 @@
 'use strict';
 //Get data from local storage
-const highScore = localStorage.getItem('scoreArray');
+const highScore = localStorage.getItem('newAdventurer');
 
 //Parsing the stringified data
 let parsedScore = JSON.parse(highScore);
 
-
+console.log(parsedScore);
 //Sort the score from largest to smallest
 let sortedScore = parsedScore.sort((a,b)=> b-a);
 
